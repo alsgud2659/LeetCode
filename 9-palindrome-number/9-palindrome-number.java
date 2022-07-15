9d975fb216adfdf715742b816d703695e95243d5
LeeMinHyung <94655599+alsgud2659@users.noreply.github.com>
2022-07-15 17:57:54 +0900

    Time: 12 ms (78.52%), Space: 45.6 MB (20.72%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        
+        String s = Integer.toString(x);
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end){
+            if(s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
