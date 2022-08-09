852c388cf98a5344351fed91e2b92a52ff8e85b2
LeeMinHyung <94655599+alsgud2659@users.noreply.github.com>
2022-08-09 15:41:18 +0900

    Update 509-fibonacci-number.java

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,15 +1,18 @@
 class Solution {
     public int fib(int n) {
+        // n이 0 이거나 1일때의 처리
         if(n == 0) {
             return 0;
         }
         if(n == 1) {
             return 1;
         }
+        
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
         
+        // 메모이제이션
         for(int i = 2; i < dp.length; i++) {
             dp[i] = dp[i - 2] + dp[i - 1];
         }
